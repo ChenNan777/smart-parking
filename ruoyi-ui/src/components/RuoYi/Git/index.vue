4b13f2273f0c64704410cab66defc47b63503a85
chennan <905394587@qq.com>
2023-10-15 22:49:21 +0800

    init

---
+++ ruoyi-ui/src/components/RuoYi/Git/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Git",
+  data() {
+    return {
+      url: "https://github.com/ChenNan777/smart-parking",
+    };
+  },
+  methods: {
+    goto() {
+      window.open(this.url);
+    },
+  },
+};
+</script>
